de934ae427297be3dc90231c33494e34d633c38c
Magesh <mageshsk16@gmail.com>
2021-05-27 13:31:12 +0530

    Add dynamic change  in dropdown

--- src/App.js
+++ src/App.js
@@ -1,54 +1,50 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { MenuItem, FormControl, Select } from "@material-ui/core";
 
 function App() {
-  const [countries, setCountries] =  useState([
-    'USA','UK','India'
-  ]);
+  const [countries, setCountries] = useState([]);
+  const[country,setCountry] = useState('worldwide');
 
-    useEffect(()=>{
-      const getCountriesData = async() =>{
-        await fetch ("https://disease.sh/v3/covid-19/countries")
-        .then ((response) => response.json())
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
         .then((data) => {
-          const countries = data.map((country) =>(
-            {
-              name: country.country,
-              value: country.countryInfo.iso2
-            }
-          ));
-            setCountries(countries);
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
 
-        })
-      
-      }
-      getCountriesData();
-    }, []);
+const onCountryChange = async (event)=>{
+  const countryCode = event.target.value;
 
+  console.log("yoooooo",countryCode); 
+
+  setCountry(countryCode);
+}
 
   return (
     <div className="app">
-      <div className='app_header'>
-      <h1>COVID-19 TRACKER</h1>
-      <FormControl className="app_dropdown">
-        <Select variant="outlined" value="abc">
-
-        {
-          countries.map(country =>(
-            <MenuItem value={country.value}>{country.name}</MenuItem>
-          ))
-        }
-
-          {/* <MenuItem value="worldwide">worldwide</MenuItem>
-          <MenuItem value="worldwide">opt 2</MenuItem>
-          <MenuItem value="worldwide">opt 3</MenuItem>
-          <MenuItem value="worldwide">opt 4</MenuItem> */}
-        </Select>
-      </FormControl>
+      <div className="app_header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" 
+          onChange={onCountryChange}
+          value={country}>
+            <MenuItem value='worldwide'>Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+            ;
+          </Select>
+        </FormControl>
       </div>
-
-     
 
       {/* Header */}
       {/* Title & select input dropdown field*/}
